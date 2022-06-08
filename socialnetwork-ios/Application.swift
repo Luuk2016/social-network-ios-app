be96067620cabf5be3b29caaa04c07d502c7374f
Luuk Kenselaar <luuk@lkenselaar.dev>
2022-06-08 12:22:20 +0200

    Add first files

---
+++ socialnetwork-ios/Application.swift
@@ -0,0 +1,17 @@
+//
+//  socialnetwork_iosApp.swift
+//  socialnetwork-ios
+//
+//  Created by Luuk Kenselaar on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Application: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
